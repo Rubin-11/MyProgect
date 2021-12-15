00b11c1af069ee1e88bc2c4c0c7cc0c34cd2b24e
Sergey Misharin <misharin_sergey-work@mail.ru>
2021-12-15 22:36:35 +0300

    preparation for work

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>App</h1>
+      </header>
+    </div>
+  );
+}
